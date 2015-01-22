b4346a6845acb8c43a9648e80baeca46581996cd
unknown <nabeelm@FTLLNABEELM07.citrite.net>
2015-01-22 13:49:35 -0500

    Changes

---
+++ R_Training.R
@@ -0,0 +1,14 @@
+# Testing first command using github project 
+1 + 1 
+
+getwd()
+library(ggplot2)
+search() # To find all the functions in the package
+
+find.package("ggplot2") # Find if the package is installed on your computer
+
+find.pacakge("caret")
+
+install.packages("KernSmooth")
+
+library(KernSmooth)
