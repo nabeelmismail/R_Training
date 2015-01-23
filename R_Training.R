d8ca9b4526ef38fc118f0be7ddd19daa009df912
unknown <nabeelm@FTLLNABEELM07.citrite.net>
2015-01-23 11:17:44 -0500

    Some additinal Notes

--- R_Training.R
+++ R_Training.R
@@ -12,3 +12,15 @@ find.pacakge("caret")
 install.packages("KernSmooth")
 
 library(KernSmooth)
+
+x <- 1:20 #Entering range of values in vector
+# Everything you see in R are objects 
+
+class(x)
+
+as.numeric(x)
+class(x)
+as.character(x)
+class(x)
+
+as.logical(x)
