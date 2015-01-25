66e903c9582ac1d11b8dd8b6651b56c3dc1e8469
unknown <nabeelm@FTLLNABEELM07.citrite.net>
2015-01-25 11:05:33 -0500

    Exploratory Data Analytis

---
+++ Exploratory Data Analysis.R
@@ -0,0 +1,19 @@
+#Principles of exploratory data analysis
+# 1.Show evidence , compared to what , show comparisons 
+# Evidence for a hypothesis is always relative to another competing theory
+
+# 2.Show causality, mechanism, explanation, systematic structure
+# What is the causal framework for thinking about a question
+
+# 3. Show multivariate data
+# Multivariate = more than 2 variables
+# Need to escape flat land
+
+# 4.Integration of evidence
+ # Completely integrate words, numbers , image , diagrams
+ # Data graphics should make use of many modes of data presentation
+ # Don't let the tool drive the analysis
+
+#5 Describe and document the evidence with appropriate labels, scales , sources etc
+
+
