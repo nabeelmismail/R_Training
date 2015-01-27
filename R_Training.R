ac53144b0819ccb024c5fbebc9bd575c57528a52
unknown <nabeelm@FTLLNABEELM07.citrite.net>
2015-01-27 00:46:06 -0500

    Random forest and Decision Trees

--- R_Training.R
+++ R_Training.R
@@ -104,3 +104,18 @@ predict(fit, type="vector") # level numbers
 predict(fit, type="class")  # factor
 
 predict(fit, type="matrix") # level number, class frequencies, probabilities
+
+#Random Forest 
+install.packages("randomForest")
+library(randomForest)
+
+fmi<-randomForest(Species~.,iris,mtry=3,ntry=500)
+
+predict(fmi, type="prob")
+
+data(iris)
+set.seed(111)
+ind <- sample(2, nrow(iris), replace = TRUE, prob=c(0.8, 0.2))
+iris.rf <- randomForest(Species ~ ., data=iris[ind == 1,])
+iris.pred <- predict(iris.rf, iris[ind == 2,])
+table(observed = iris[ind==2, "Species"], predicted = iris.pred, type = "prob")
